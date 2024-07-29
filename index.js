0a2380368deb58abc481394e9919474568ff01be
poteto <naonyan1135@gmail.com>
2024-07-29 14:40:48 +0900

    hoee

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const shelterRoutes = require("./routes/shelter")
 
 
-app.use("/",shelterRoutes) //* 第一引数のが自動で付けられる　このばあいだとrouteの時に
+// app.use("/",shelterRoutes) //* 第一引数のが自動で付けられる　このばあいだとrouteの時に
 app.use("/shelters",shelterRoutes) //* これだと/shelters/sheltersになっちゃうよ
 
 app.listen(3000,()=>{
--- routes/shelter.js
+++ routes/shelter.js
@@ -1,16 +1,20 @@
 const express = require("express")
 const router = express.Router()
 
-router.get("/shelters",(req,res)=>{
-res.send("all shelters")
+router.get("/", (req, res) => {
+  res.send("all shelters")
 })
 
-router.post("/shelters",(req,re)=>{
-res.send("create shelter")
+router.post("/", (req, re) => {
+  res.send("create shelter")
 })
 
-router.get("/shelters/:id/edit",(req,res)=>{
+router.get("/:id/edit", (req, res) => {
   res.send("edit")
+})
+
+router.get("/:id", (req, res) => {
+  res.send("HI")
 })
 
 module.exports = router
